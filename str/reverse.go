9ad5f39de449ca8f5e76d4084a710ce909fcaf06
James Pirruccello <james@parchment.com>
2013-01-24 19:09:34 -0500

    Reverse a string.

---
+++ str/reverse.go
@@ -0,0 +1,16 @@
+package str
+
+//Reverses a string
+//Inspired by http://golang.org/doc/effective_go.html#for
+func Reverse(s string) string{
+    //Initialize our output as a byte-ified version of string s
+    o := []byte(s)
+    
+    //Loop over the string, swapping the Nth and the (len(s)-1 - N)th 
+    // elements until you reach the midpoint.
+    for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+        o[i], o[j] = o[j], o[i]
+    }
+
+    return string(o)
+}
