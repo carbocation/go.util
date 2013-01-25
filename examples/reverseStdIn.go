e0f4e19e26d96c7182e8d392b80d9ee98e1ba78b
James Pirruccello <james@parchment.com>
2013-01-24 20:12:58 -0500

    Documenting TODOs

--- examples/reverseStdIn.go
+++ examples/reverseStdIn.go
@@ -7,6 +7,11 @@ import (
     "github.com/carbocation/util.git/str"
 )
 
+//Currently reverses ONLY THE FIRST LINE PASSED TO IT
+//@TODO@ Allow it to read an indefinite number of lines, reversing each in turn
+//@TODO@ Use goroutines or channels or whatever to concurrently read in lines 
+// and output reversed lines instead of doing these things sequentially, using 
+// multiple cores
 func main() {
     stdIn := bufio.NewReader(os.Stdin)
     input, _ := stdIn.ReadString('\n')
