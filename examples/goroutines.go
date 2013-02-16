45a17c9b0e9a0fbf8cdd911f43f5ddc7970a9b4a
James Pirruccello <james@parchment.com>
2013-02-16 13:51:27 -0500

    Now the goroutines example forms an explicit map-reduce framework.

--- examples/goroutines.go
+++ examples/goroutines.go
@@ -3,6 +3,7 @@ package main
 import (
     "fmt"
     "runtime"
+    //"math"
 )
 
 func main() {
@@ -11,35 +12,62 @@ func main() {
     runtime.GOMAXPROCS(numCpu)
     
     //Number of integers that will be in the channel
-    var N int = 1e7
+    //We will start at 0, so if N==1 then you will sum 0, if N==3 you will sum 1+2, etc.
+    var N int64 = 1e8
 
     //Create our channel with enough space to hold all integers at once
-    c := make(chan int, N)
+    c := make(chan int64, N)
 
     //In a separate thread, fill the channel
-    go func() {
-        //defer close(c)
-        start := 0
-        stop := N
-        fillChannel(c, start, stop)
-    }()
+    //Instead of deferring a close in the filling channel, we'll explicitly keep 
+    //track of the number of expected channel elements so that we can drain them 
+    //via for loop rather than range loop.
+
+    //This now lets us create multiple goroutines to fill the channel, instead of 
+    //relying on deferred closure from the filling channel which would limit us to 1
+    //filling goroutine at a time.
+
+    //This is basically the map step (one mapper)
+    var start, stop int64 = 0, 0
+    numGoRoutines := int64(numCpu * 4)
+    for i := 0; int64(i) < numGoRoutines; i++ {
+        start = int64(i) * N/numGoRoutines
+        stop = int64(i+1) * N/numGoRoutines
+
+        go func(start int64, stop int64) {
+            fmt.Println("Filling channel from",start,"to",stop)
+            fillChannel(c, start, stop)
+        }(start, stop)
+    }
+    if stop < N {
+        fmt.Println("Finally filling channel from",stop,"to",N)
+        go fillChannel(c, stop, N)
+    }
 
     //In this thread, drain the channel (for example, you could print the records here)
+    //This is basically the reduce step (one reducer)
     sum := drainChannel(c, N)
 
     fmt.Println("By manual counting, the sum of all integers from 0 to",(N-1),"is",sum)
 }
 
-func fillChannel(c chan int, start int, stop int) {
-    for i := start; i < stop; i++ {
-        c <- i
+func fillChannel(c chan int64, start int64, stop int64) {
+    for i := start; int64(i) < stop; i++ {
+        c <- int64(i)
     }
 }
 
-func drainChannel(c chan int, N int) int64 {
+func drainChannel(c chan int64, N int64) int64 {
     var x int64 = 0
-    for i := 0; i < N; i++ {
-        x += int64(<-c)
+    for i := 0; int64(i) < N; i++ {
+        this := <-c
+        x += int64(this)
+        
+        /*
+        if math.Mod(float64(i),1e3) == 0.0 {
+            fmt.Println("Draining",this)
+        }
+        */
     }
 
     return x
