b2a512b897532cb0086dedc0bd5ce7e52a22dae7
James Pirruccello <james@parchment.com>
2013-02-21 13:33:49 -0700

    Using tree nomenclature.

--- datatypes/binarytree/binarytree.go
+++ datatypes/binarytree/binarytree.go
@@ -6,152 +6,152 @@
 
 // Package binarytree implements a binary tree, derived from the container/list code
 //
-// To iterate over a list (where l is a *List):
+// To iterate over a tree (where l is a *Tree):
 //	for e := l.Front(); e != nil; e = e.Next() {
 //		// do something with e.Value
 //	}
 //
 package binarytree
 
-// Element is an element in the linked list.
+// Element is an element in the tree.
 type Element struct {
-	// Next and previous pointers in the doubly-linked list of elements.
-	// The front of the list has prev = nil, and the back has sibling = nil.
-	next, prev *Element
+	// Next and previous pointers in the doubly-linked tree of elements.
+	// The root of the tree has parent = nil, and the leaf of a tree has child = nil.
+	child, parent *Element
 
-	// The list to which this element belongs.
-	list *List
+	// The tree to which this element belongs.
+	tree *Tree
 
-	// The contents of this list element.
+	// The contents of this tree element.
 	Value interface{}
 }
 
-// Next returns the next list element or nil.
-func (e *Element) Next() *Element { return e.next }
+// Next returns the child tree element or nil.
+func (e *Element) Next() *Element { return e.child }
 
-// Prev returns the previous list element or nil.
-func (e *Element) Prev() *Element { return e.prev }
+// Prev returns the previous tree element or nil.
+func (e *Element) Prev() *Element { return e.parent }
 
-// List represents a doubly linked list.
-// The zero value for List is an empty list ready to use.
-type List struct {
+// Tree represents a doubly linked tree.
+// The zero value for Tree is an empty tree ready to use.
+type Tree struct {
 	front, back *Element
 	len         int
 }
 
-// Init initializes or clears a List.
-func (l *List) Init() *List {
+// Init initializes or clears a Tree.
+func (l *Tree) Init() *Tree {
 	l.front = nil
 	l.back = nil
 	l.len = 0
 	return l
 }
 
-// New returns an initialized list.
-func New() *List { return new(List) }
+// New returns an initialized tree.
+func New() *Tree { return new(Tree) }
 
-// Front returns the first element in the list.
-func (l *List) Front() *Element { return l.front }
+// Front returns the first element in the tree.
+func (l *Tree) Front() *Element { return l.front }
 
-// Back returns the last element in the list.
-func (l *List) Back() *Element { return l.back }
+// Back returns the last element in the tree.
+func (l *Tree) Back() *Element { return l.back }
 
-// Remove removes the element from the list
+// Remove removes the element from the tree
 // and returns its Value.
-func (l *List) Remove(e *Element) interface{} {
+func (l *Tree) Remove(e *Element) interface{} {
 	l.remove(e)
-	e.list = nil // do what remove does not
+	e.tree = nil // do what remove does not
 	return e.Value
 }
 
-// remove the element from the list, but do not clear the Element's list field.
-// This is so that other List methods may use remove when relocating Elements
-// without needing to restore the list field.
-func (l *List) remove(e *Element) {
-	if e.list != l {
+// remove the element from the tree, but do not clear the Element's tree field.
+// This is so that other Tree methods may use remove when relocating Elements
+// without needing to restore the tree field.
+func (l *Tree) remove(e *Element) {
+	if e.tree != l {
 		return
 	}
-	if e.prev == nil {
-		l.front = e.next
+	if e.parent == nil {
+		l.front = e.child
 	} else {
-		e.prev.next = e.next
+		e.parent.child = e.child
 	}
-	if e.next == nil {
-		l.back = e.prev
+	if e.child == nil {
+		l.back = e.parent
 	} else {
-		e.next.prev = e.prev
+		e.child.parent = e.parent
 	}
 
-	e.prev = nil
-	e.next = nil
+	e.parent = nil
+	e.child = nil
 	l.len--
 }
 
-func (l *List) insertBefore(e *Element, mark *Element) {
-	if mark.prev == nil {
-		// new front of the list
+func (l *Tree) insertBefore(e *Element, mark *Element) {
+	if mark.parent == nil {
+		// new front of the tree
 		l.front = e
 	} else {
-		mark.prev.next = e
+		mark.parent.child = e
 	}
-	e.prev = mark.prev
-	mark.prev = e
-	e.next = mark
+	e.parent = mark.parent
+	mark.parent = e
+	e.child = mark
 	l.len++
 }
 
-func (l *List) insertAfter(e *Element, mark *Element) {
-	if mark.next == nil {
-		// new back of the list
+func (l *Tree) insertAfter(e *Element, mark *Element) {
+	if mark.child == nil {
+		// new back of the tree
 		l.back = e
 	} else {
-		mark.next.prev = e
+		mark.child.parent = e
 	}
-	e.next = mark.next
-	mark.next = e
-	e.prev = mark
+	e.child = mark.child
+	mark.child = e
+	e.parent = mark
 	l.len++
 }
 
-func (l *List) insertFront(e *Element) {
+func (l *Tree) insertFront(e *Element) {
 	if l.front == nil {
-		// empty list
+		// empty tree
 		l.front, l.back = e, e
-		e.prev, e.next = nil, nil
+		e.parent, e.child = nil, nil
 		l.len = 1
 		return
 	}
 	l.insertBefore(e, l.front)
 }
 
-func (l *List) insertBack(e *Element) {
+func (l *Tree) insertBack(e *Element) {
 	if l.back == nil {
-		// empty list
+		// empty tree
 		l.front, l.back = e, e
-		e.prev, e.next = nil, nil
+		e.parent, e.child = nil, nil
 		l.len = 1
 		return
 	}
 	l.insertAfter(e, l.back)
 }
 
-// PushFront inserts the value at the front of the list and returns a new Element containing the value.
-func (l *List) PushFront(value interface{}) *Element {
+// PushFront inserts the value at the front of the tree and returns a new Element containing the value.
+func (l *Tree) PushFront(value interface{}) *Element {
 	e := &Element{nil, nil, l, value}
 	l.insertFront(e)
 	return e
 }
 
-// PushBack inserts the value at the back of the list and returns a new Element containing the value.
-func (l *List) PushBack(value interface{}) *Element {
+// PushBack inserts the value at the back of the tree and returns a new Element containing the value.
+func (l *Tree) PushBack(value interface{}) *Element {
 	e := &Element{nil, nil, l, value}
 	l.insertBack(e)
 	return e
 }
 
 // InsertBefore inserts the value immediately before mark and returns a new Element containing the value.
-func (l *List) InsertBefore(value interface{}, mark *Element) *Element {
-	if mark.list != l {
+func (l *Tree) InsertBefore(value interface{}, mark *Element) *Element {
+	if mark.tree != l {
 		return nil
 	}
 	e := &Element{nil, nil, l, value}
@@ -160,8 +160,8 @@ func (l *List) InsertBefore(value interface{}, mark *Element) *Element {
 }
 
 // InsertAfter inserts the value immediately after mark and returns a new Element containing the value.
-func (l *List) InsertAfter(value interface{}, mark *Element) *Element {
-	if mark.list != l {
+func (l *Tree) InsertAfter(value interface{}, mark *Element) *Element {
+	if mark.tree != l {
 		return nil
 	}
 	e := &Element{nil, nil, l, value}
@@ -169,29 +169,29 @@ func (l *List) InsertAfter(value interface{}, mark *Element) *Element {
 	return e
 }
 
-// MoveToFront moves the element to the front of the list.
-func (l *List) MoveToFront(e *Element) {
-	if e.list != l || l.front == e {
+// MoveToFront moves the element to the front of the tree.
+func (l *Tree) MoveToFront(e *Element) {
+	if e.tree != l || l.front == e {
 		return
 	}
 	l.remove(e)
 	l.insertFront(e)
 }
 
-// MoveToBack moves the element to the back of the list.
-func (l *List) MoveToBack(e *Element) {
-	if e.list != l || l.back == e {
+// MoveToBack moves the element to the back of the tree.
+func (l *Tree) MoveToBack(e *Element) {
+	if e.tree != l || l.back == e {
 		return
 	}
 	l.remove(e)
 	l.insertBack(e)
 }
 
-// Len returns the number of elements in the list.
-func (l *List) Len() int { return l.len }
+// Len returns the number of elements in the tree.
+func (l *Tree) Len() int { return l.len }
 
-// PushBackList inserts each element of ol at the back of the list.
-func (l *List) PushBackList(ol *List) {
+// PushBackTree inserts each element of ol at the back of the tree.
+func (l *Tree) PushBackTree(ol *Tree) {
 	last := ol.Back()
 	for e := ol.Front(); e != nil; e = e.Next() {
 		l.PushBack(e.Value)
@@ -201,8 +201,8 @@ func (l *List) PushBackList(ol *List) {
 	}
 }
 
-// PushFrontList inserts each element of ol at the front of the list. The ordering of the passed list is preserved.
-func (l *List) PushFrontList(ol *List) {
+// PushFrontTree inserts each element of ol at the front of the tree. The ordering of the passed tree is preserved.
+func (l *Tree) PushFrontTree(ol *Tree) {
 	first := ol.Front()
 	for e := ol.Back(); e != nil; e = e.Prev() {
 		l.PushFront(e.Value)
