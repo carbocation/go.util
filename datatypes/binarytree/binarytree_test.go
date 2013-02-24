3256fbd084a4a68471c4562125ea202e6b256fc4
James Pirruccello <james@parchment.com>
2013-02-24 13:53:32 -0500

    Fixing the binary tree test.

--- datatypes/binarytree/binarytree_test.go
+++ datatypes/binarytree/binarytree_test.go
@@ -2,7 +2,6 @@ package binarytree
 
 import (
     "testing"
-    "fmt"
 )
 
 // In this test, we are creating an unbalanced binary tree
@@ -13,16 +12,16 @@ func TestTree(t *testing.T) {
     e.Left().PushLeft("Root:Left:Left")
     e.Left().PushRight("Root:Left:Right")
     e.Left().Right().PushRight("Root:Left:Right:Right")
-    
-    fmt.Printf("%v, %v, %v\n",e)
 
     //Get a channel with our output and print it out line by line
     x := Walker(e)
     val := ""
     for y := range x {
-        fmt.Println(y)
         val += y.(string) + "::"
     }
 
-    fmt.Println(val)
+    expected := "Root::Root:Left::Root:Left:Left::Root:Left:Right::Root:Left:Right:Right::Root:Right::"
+    if val != expected {
+        t.Errorf("Unexpected value. Received %s, expected %s.", val, expected)
+    }
 }
